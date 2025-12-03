f1b5d6fc4f7c3ba82bc6b1210b72561f2166dddf
Marika <marikaserafina@gmail.com>
2025-12-04 00:01:54 +0100

    add 1.1

---
+++ Esercizio1.1.py
@@ -0,0 +1,13 @@
+
+#Creare una lista server contenente: ["web01", "db01", "cache01"]
+#Aggiungere "backup01" alla fine
+#Inserire "proxy01" all'inizio (indice 0)
+#Rimuovere "cache01"
+#Stampare la lista finale e la sua lunghezza
+
+server :list[str]=["web01","db01","cache01"]
+server.append("backup01")
+server.insert(0, "proxy01")
+server.pop(3)
+print(server)
+print(len(server))
