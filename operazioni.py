db51541cdd3ea8154ae028947b28a6df0ef79b47
Marika <marikaserafina@gmail.com>
2025-12-05 11:57:53 +0100

    add moduli

---
+++ operazioni.py
@@ -0,0 +1,11 @@
+# Contenuto di operazioni.py
+
+def somma(a:int, b:int) -> int:
+    """Somma due numeri"""
+    return a + b
+
+def moltiplica(a:int, b:int) -> int:
+    """Moltiplica due numeri"""
+    return a * b
+
+PI_GRECO: float = 3.14159
