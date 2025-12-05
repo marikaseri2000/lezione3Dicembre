c9f130c0ceb1e84a2455f865b47f03617bfb3de3
Marika <marikaserafina@gmail.com>
2025-12-05 12:17:12 +0100

    add file

---
+++ matematica/__init__.py
@@ -0,0 +1,5 @@
+from .operazioni import moltiplica,somma
+from .operazioni_complesse import sommatoria
+
+#mi esporta fuori quello che mi serve anzichè tutte le funzionalità
+__all__=[moltiplica, somma]
---
+++ matematica/operazioni_complesse.py
@@ -0,0 +1,4 @@
+from matematica.operazioni import somma
+
+def sommatoria(a:int)-> int:
+    return somma(a,2)
